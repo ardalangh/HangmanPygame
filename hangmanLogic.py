033eae39748002603003a8db4eda99477db862e4
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-03 17:28:50 -0800

    end of session

--- hangmanLogic.py
+++ hangmanLogic.py
@@ -1,6 +1,17 @@
-wrd  = "apple"
-displayList = []
-wrongAns = []
+import random
+
+
+file1 = open("words.txt", "r")
+words = file1.readlines()
+randomWrd = random.choice(words)
+print(randomWrd)
+
+
+
+
+
+
+# wrd  = "apple"
 
 
 
@@ -9,42 +20,55 @@ wrongAns = []
 
 
 
-def didWin(displayList):
-    return "-" not in displayList
-
-
-for character in wrd:
-    displayList.append("-")
-
-print("Hello welcome to hangman guess a letter")
-print(displayList)
 
 
 
-while (True):
 
-    if len(wrongAns) == 6:
-        print("you lost")
-        break
+# displayList = []
+# wrongAns = []
 
 
-    if didWin(displayList):
-        print("you won")
-        break
 
 
-    guess = input("choose a lowercase letter: ")
-    for i, character in enumerate(wrd):    
-        if character == guess:
-            displayList[i] = character
+
+
+
+
+# def didWin(displayList):
+#     return "-" not in displayList
+
+
+# for character in wrd:
+#     displayList.append("-")
+
+# print("Hello welcome to hangman guess a letter")
+# print(displayList)
+
+
+
+# while (True):
+#     if len(wrongAns) == 6:
+#         print("you lost")
+#         break
+
+
+#     if didWin(displayList):
+#         print("you won")
+#         break
+
+
+#     guess = input("choose a lowercase letter: ")
+#     for i, character in enumerate(wrd):    
+#         if character == guess:
+#             displayList[i] = character
         
     
-    if guess not in wrd:
-        print("wrong")
-        wrongAns.append(guess)
-        print(wrongAns)
-    else:
-        print(displayList)
+#     if guess not in wrd:
+#         print("wrong")
+#         wrongAns.append(guess)
+#         print(wrongAns)
+#     else:
+#         print(displayList)
             
         
         
