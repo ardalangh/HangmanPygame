d2ac8eaa922f43c4f43f1462c6f350558ed855d4
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-04-21 16:59:51 -0700

    fixed the visuals.py codes that were deleted

--- Buttons.py
+++ Buttons.py
@@ -1,4 +1,14 @@
 class Buttons:
+
+    dic = {
+    "a" : (10, 10),
+    "b" : (20, 10),
+    "d" : (30, 10),
+    "e" : (10, 10),
+    "r" : (10, 10),
+    "a" : (10, 10),
+}
+
     def __init__(self):
         alphabet_string = string.ascii_lowercase
         self.letters = list(alphabet_string)
--- visuals.py
+++ visuals.py
@@ -2,48 +2,22 @@ import pygame
 from screeninfo import get_monitors
 
 
-class Buttons:
-    def __init__(self):
-        alphabet_string = string.ascii_lowercase
-        self.letters = list(alphabet_string)
-        self.h = 20
-        self.w = 30
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # COLORS
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
 HEIGHT = get_monitors()[0].height
 WIDTH = get_monitors()[0].width
 
 
+pygame.init()
+surface = pygame.display.set_mode((HEIGHT, WIDTH)))
 
 
-dic = {
-    "a" : (10, 10),
-    "b" : (20, 10),
-    "d" : (30, 10),
-    "e" : (10, 10),
-    "r" : (10, 10),
-    "a" : (10, 10),
-}
-
-
-
-buttons = Buttons()
+running = True
+clock = pygame.time.Clock()
+while running:
+    pygame.event.pump()
+    event = pygame.event.wait()
+    if event.type == pygame.QUIT:
+        running = False
