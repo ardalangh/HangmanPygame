5fe8c5f090b965356c615abe29187c2acd92c2ba
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-04-21 16:53:02 -0700

    buttomn

--- visuals.py
+++ visuals.py
@@ -11,18 +11,6 @@ class Buttons:
 
 
 
-class Button:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def draw(self, surface):
-        font = pygame.font.Font('freesansbold.ttf', 32)
-        text = font.render(self.letter, True, (255, 255, 255), (0,0,0))
-        textRect = text.get_rect()
-        textRect.center = (200 // 2, 200 // 2)
-        pygame.draw.rect(surface, BLACK, (self.x, self.y, self.w, self.h), 1)
-        surface.blit(text, textRect)
 
 
 
